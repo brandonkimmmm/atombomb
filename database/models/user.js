f652c991c9347fdd34566edd0308f5e7e2b9a757
Brandon Kim <bkim2490@gmail.com>
2020-09-28 22:05:24 +0900

    database init

---
+++ database/models/user.js
@@ -0,0 +1,21 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+	const User = sequelize.define('User', {
+		name: {
+			type: DataTypes.STRING,
+			required: true
+		},
+		email: {
+			type: DataTypes.STRING,
+			required: true
+		},
+		password: {
+			type: DataTypes.STRING,
+			required: true
+		}
+	}, {});
+	User.associate = function(models) {
+		// associations can be defined here
+	};
+	return User;
+};
