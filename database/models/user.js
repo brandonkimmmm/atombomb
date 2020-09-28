ec52115a9b8f04a601495139fd7d45a4349f0499
Brandon Kim <bkim2490@gmail.com>
2020-09-28 22:28:05 +0900

    salt passwrod

--- database/models/user.js
+++ database/models/user.js
@@ -1,4 +1,8 @@
 'use strict';
+
+const bcrypt = require('bcryptjs');
+require('dotenv').config();
+
 module.exports = (sequelize, DataTypes) => {
 	const User = sequelize.define('User', {
 		name: {
@@ -13,7 +17,19 @@ module.exports = (sequelize, DataTypes) => {
 			type: DataTypes.STRING,
 			required: true
 		}
-	}, {});
+	}, {
+		hooks: {
+			beforeCreate: (user) => {
+				const salt = bcrypt.genSaltSync(process.env.SALT_ROUNDS);
+				user.password = bcrypt.hashSync(user.password, salt);
+			}
+		},
+		instanceMethods: {
+			validPassword(password) {
+				return bcrypt.compare(password, this.password)
+			}
+		}
+	});
 	User.associate = function(models) {
 		// associations can be defined here
 	};
